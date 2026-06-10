fcb028f6b3ca52ca9463fa5660c5ff414a6875aa
BNR <nrajithabhogadi@gmail.com>
2026-06-10 11:45:23 -0500

    Deleted Test Case from SapceX Project In SpalshViewController

--- SpaceXRockets/Controller/SplashViewController.swift
+++ SpaceXRockets/Controller/SplashViewController.swift
@@ -11,20 +11,18 @@ class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
-
+            
             let listVC = storyboard.instantiateViewController(
                 withIdentifier: "RocketListViewController"
             )
-
+            
             let navController = UINavigationController(rootViewController: listVC)
             navController.modalPresentationStyle = .fullScreen
-
+            
             self.present(navController, animated: true)
         }
-        
-    //Test Commant
     }
 }
